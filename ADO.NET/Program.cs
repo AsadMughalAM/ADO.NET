7482d2ce11c8cb4f8568e14d9fb84c277a649f70
AsadMughalAM <am127955@gmail.com>
2024-08-12 07:54:35 +0500

    Add project files.

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,92 @@
+﻿using ADO.NET.Entities;
+using ADO.NET.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("Select from the options below\n1.Retrieve\n2.Create\n3.Update\n4.Delete");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        var list = EmployeeRepository.GetData();
+                        foreach (var item in list)
+                        {
+                            Console.WriteLine($"{item.Id}\t|\t{item.Name}\t|\t{item.City}\t|\t{item.Address}");
+                        }
+                        break;
+
+                    case 2:
+                        var obj = new Employee();
+
+                        Console.Write("Enter Id: ");
+                        obj.Id = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter Name: ");
+                        obj.Name = Console.ReadLine();
+
+                        Console.Write("Enter City: ");
+                        obj.City = Console.ReadLine();
+
+                        Console.Write("Enter Address: ");
+                        obj.Address = Console.ReadLine();
+
+                        bool result = EmployeeRepository.CreatePerson(obj);
+                        string message = result ? "Person created!" : "Failed something went wrong";
+                        Console.WriteLine(message);
+                        break;
+
+                    case 3:
+                        var Obj = new Employee();
+
+                        Console.Write("Enter Id: ");
+                        Obj.Id = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter Name: ");
+                        Obj.Name = Console.ReadLine();
+
+                        Console.Write("Enter City: ");
+                        Obj.City = Console.ReadLine();
+
+                        Console.Write("Enter Address: ");
+                        Obj.Address = Console.ReadLine();
+
+                        bool result1 = EmployeeRepository.UpdatePerson(Obj);
+                        string message1 = result1 ? "Person updated!" : "Failed something went wrong";
+                        Console.WriteLine(message1);
+                        break;
+
+                    case 4:
+                        var obj1 = new Employee();
+                        Console.WriteLine("Enter the id of the person you want to delete");
+                        obj1.Id = int.Parse(Console.ReadLine());
+
+                        bool result2 = EmployeeRepository.DeletePerson(obj1);
+                        string message2 = result2 ? "Person deleted!" : "Failed something went wrong";
+                        Console.WriteLine(message2);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Number");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue?");
+                answer = Console.ReadLine();
+            }
+            while (answer != "no");
+        }
+    }
+}
+
+
---
+++ ADO.NET/Repositories/EmployeeRepository.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADO.NET.Entities;
+
+namespace ADO.NET.Repositories
+{
+    public static class EmployeeRepository
+    {
+        public static string _connectionString { get; set; } = @"Data Source=DESKTOP-3J635BH;Initial Catalog=Employee;Integrated Security=True;Encrypt=False";
+
+        public static List<Employee> GetData()
+        {
+            string commandString = "GetEmployees";
+            var employees = new List<Employee>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var command = new SqlCommand(commandString, connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                var dataAdapter = new SqlDataAdapter(command);
+                var dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    var employee = new Employee()
+                    {
+                        Id = (int)row["Id"],
+                        Name = (string)row["Name"],
+                        City = (string)row["City"],
+                        Address = (string)row["Address"],
+                    };
+
+                    employees.Add(employee);
+                }
+            }
+
+            return employees;
+        }
+
+        public static bool CreatePerson(Employee obj)
+        {
+            string insertQuery = "AddNewEmpDetails";
+
+            var connection = new SqlConnection(_connectionString);
+            
+
+                connection.Open();
+                var insertCommand = new SqlCommand(insertQuery, connection);
+
+                insertCommand.CommandType = CommandType.StoredProcedure;
+                insertCommand.Parameters.AddWithValue("@Name", obj.Name);
+                insertCommand.Parameters.AddWithValue("@City", obj.City);
+                insertCommand.Parameters.AddWithValue("@Address", obj.Address);
+                insertCommand.ExecuteNonQuery();
+            
+            return true;
+        }
+
+
+        public static bool UpdatePerson(Employee obj)
+        {
+            string updateQuery = "UpdateEmpDetails";
+
+            var connection = new SqlConnection(_connectionString);
+
+            connection.Open();
+            var updateCommand = new SqlCommand(updateQuery, connection);
+
+            updateCommand.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter p1 = new SqlParameter("@Id", SqlDbType.Int);
+            p1.Value = obj.Id;
+            updateCommand.Parameters.Add(p1);
+
+            updateCommand.ExecuteNonQuery();
+
+            return true;
+        }
+
+
+
+
+        public static bool DeletePerson(Employee obj)
+        {
+            string deleteQuery = "DeleteEmpById";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+
+                connection.Open();
+                var deleteCommand = new SqlCommand(deleteQuery, connection);
+                
+                    deleteCommand.CommandType = CommandType.StoredProcedure;
+
+                    SqlParameter param1 = new SqlParameter
+                    {
+                        ParameterName = "@id",
+                        SqlDbType = SqlDbType.Int,
+                        Value = obj.Id
+
+                    };
+                    deleteCommand.Parameters.Add(param1);
+                    deleteCommand.Parameters.AddWithValue("@Id", obj.Id);
+
+                    deleteCommand.ExecuteNonQuery();
+                
+                return true;
+            }
+        }
+    }
+}
+
+
+
+
+
+
